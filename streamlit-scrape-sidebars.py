bddaf7faf6b0453fbd1f28bfdc53411af9b48795
4rt10m <4rt10m013@gmail.com>
2023-08-03 18:00:48 +0100

    Update streamlit-scrape-sidebars.py

--- streamlit-scrape-sidebars.py
+++ streamlit-scrape-sidebars.py
@@ -3,20 +3,18 @@ from bs4 import BeautifulSoup
 import json
 import csv
 import streamlit as st
-import pandas as pd
-import io
 
 def scrape_sidebar_urls(urls):
     sidebar_urls = []
 
     for url in urls:
-        try:
-            response = requests.get(url)
-            response.raise_for_status()  # Check for successful response
-
+        response = requests.get(url)
+        if response.status_code == 200:
+            # Get the script tag from HTML
             soup = BeautifulSoup(response.content, 'html.parser')
             script_tag = soup.find('script', id='__NEXT_DATA__')
 
+            # Get data from the script
             if script_tag:
                 script_content = script_tag.string
                 script_json = json.loads(script_content)
@@ -26,28 +24,26 @@ def scrape_sidebar_urls(urls):
                     sidebar_items = page_props.get('sidebar', {}).get('items', [{}])
                     sidebar_type = page_props.get('sidebar', {}).get('name', '')
 
-                    if sidebar and sidebar_items:
-                        for item in sidebar_items:
-                            sidebarSectionName = item.get('name', '')
-                            heading = item.get('heading', '')
-                            items = item.get('items', [])
-                            for sidebaraurl in items:
-                                urlPathWithAncestry = sidebaraurl.get('urlPathWithAncestry', '')
-                                if urlPathWithAncestry:
-                                    sidebar_urls.append({
-                                        'URL': url,
-                                        'Heading': heading,
-                                        'SidebarURL': urlPathWithAncestry,
-                                        'Sidebar Type': sidebar_type
-                                    })
-                    else:
-                        st.warning('No sidebar found for ' + url)
+                if sidebar and sidebar_items:
+                    for item in sidebar_items:
+                        sidebarSectionName = item.get('name', '')
+                        heading = item.get('heading', '')
+                        items = item.get('items', [])
+                        for sidebaraurl in items:
+                            urlPathWithAncestry = sidebaraurl.get('urlPathWithAncestry', '')
+                            if urlPathWithAncestry:
+                                sidebar_urls.append({
+                                    'URL': url,
+                                    'Heading': heading,
+                                    'SidebarURL': urlPathWithAncestry,
+                                    'Sidebar Type': sidebar_type
+                                })
                 else:
-                    st.warning(f"No script tag with id '__NEXT_DATA__' found in {url}")
+                    st.warning('No sidebar found for ' + url)
             else:
                 st.warning(f"No script tag with id '__NEXT_DATA__' found in {url}")
-        except requests.exceptions.RequestException as e:
-            st.warning(f"Failed to fetch {url}. Error: {e}")
+        else:
+            st.warning(f"Failed to fetch {url}. Status code: {response.status_code}")
 
     return sidebar_urls
 
@@ -64,11 +60,15 @@ def main():
             sidebar_urls = scrape_sidebar_urls(urls)
             if sidebar_urls:
                 # Display the scraped data in a table
-                st.table(pd.DataFrame(sidebar_urls))
-                # Export to CSV and provide download link
-                csv_file = pd.DataFrame(sidebar_urls).to_csv(index=False)
-                st.download_button(label="Download CSV", data=io.StringIO(csv_file), file_name='enyoMZ_sidebar_urls-all-pages.csv', mime='text/csv')
-                st.success("Data exported to CSV successfully.")
+                st.table(sidebar_urls)
+                # Export to CSV
+                output_file = 'enyoMZ_sidebar_urls-all-pages.csv'
+                with open(output_file, 'w', newline='') as file:
+                    writer = csv.writer(file)
+                    writer.writerow(['URL', 'Heading', 'SidebarURL', 'Sidebar Type'])
+                    for row in sidebar_urls:
+                        writer.writerow([row['URL'], row['Heading'], row['SidebarURL'], row['Sidebar Type']])
+                st.success(f"Data exported to {output_file} successfully.")
             else:
                 st.warning('No data found. Make sure the URLs are correct and the sidebar exists.')
         else:
