90b7f38547d54edd8dbba339bf97ace88fc5a896
Artiom <4rt10m013@gmail.com>
2023-08-04 09:35:59 +0100

    links update

--- add-internal-links-to-text/add-internal-links.py
+++ add-internal-links-to-text/add-internal-links.py
@@ -4,41 +4,28 @@ def add_internal_links(text, urls_anchors):
     linked_text = text
 
     for url, anchor in urls_anchors:
-        # Find the anchor text and replace it with the Markdown link
+        # Find the anchor text and replace it with the HTML link
         anchor_regex = rf'\b{anchor}\b'
-        linked_text = linked_text.replace(anchor, f'[{anchor}]({url})')
+        linked_text = linked_text.replace(anchor, f'<a href="{url}">{anchor}</a>')
 
     return linked_text
 
 def main():
     st.title("Internal Link Generator")
 
-    input_text = """
-    This is a sample text with multiple anchors: anchor1, anchor2, and anchor3.
+    st.subheader("Add Links:")
+    urls_anchors = []
+    while True:
+        url = st.text_input("URL:")
+        anchor = st.text_input("Anchor Text:")
+        if not url or not anchor:
+            break
+        urls_anchors.append([url, anchor])
 
-    This is a sample text with multiple anchors: anchor1, anchor2, and anchor3.
-
-    This is a sample text with multiple anchors: anchor1, anchor12, and anchor3.
-    """
-
-    input_urls_anchors = [
-        ["https://example.com1", "anchor1"],
-        ["https://example.com2", "anchor2"],
-        ["https://example.com3", "anchor3"],
-        ["https://example.com31", "anchor12"]
-    ]
-
-    result = add_internal_links(input_text, input_urls_anchors)
-
-    st.subheader("Input Text:")
-    st.text_area("Input Text", value=input_text, height=200)
-
-    st.subheader("Links:")
-    for url, anchor in input_urls_anchors:
-        st.markdown(f"Link: [{anchor}]({url})")
+    result = add_internal_links("", urls_anchors)
 
     st.subheader("Result:")
-    st.text_area("Result", value=result, height=200)
+    st.text_area("Result", value=result, height=200, key="result")
 
 if __name__ == "__main__":
     main()
